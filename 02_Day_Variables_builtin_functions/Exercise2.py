a1eb9f9769aedc5b955e1ee8757e841d0fad8566
fexeduard224 <156733727+fexeduard224@users.noreply.github.com>
2024-05-08 19:45:32 +0000

    Pending changes exported from your codespace

---
+++ 02_Day_Variables_builtin_functions/Exercise2.py
@@ -0,0 +1,90 @@
+#Day 2: 30 Days of python programming'
+
+#EXERCISE 1
+
+#2. Write a python comment saying 'Day 2: 30 Days of python programming'
+#Done
+#3. Declare a first name variable and assign a value to it
+first_name = "Felix" 
+print(first_name)
+
+#4. Declare a last name variable and assign a value to it
+last_name = "Rodriguez"
+print(last_name)
+#5. Declare a full name variable and assign a value to it
+full_name = first_name + " " + last_name
+print(full_name)
+
+#6. Declare a country variable and assign a value to it
+country = "Venezuela"
+print(country)
+#7. Declare a city variable and assign a value to it
+city = "Caracas"
+print(city)
+#8. Declare an age variable and assign a value to it
+age = '31'
+print(age)
+#9. Declare a year variable and assign a value to it
+year = '1992'
+print(year)
+#10. Declare a variable is_married and assign a value to it
+married = False
+print(married)
+#11. Declare a variable is_true and assign a value to it
+
+#12. Declare a variable is_light_on and assign a value to it
+
+#13. Declare multiple variable on one line
+language, hobby, food = 'spanish', "music", "hamburger"
+print(language[1])
+#EXERCISE 2
+
+
+
+#1 Check the data type of all your variables using type() built-in function
+print(type(country))
+print(type(age))
+print(type(married))
+
+#2 Using the len() built-in function, find the length of your first name
+print(len(first_name))
+#3 Compare the length of your first name and your last name
+print(f'first name length: {len(first_name)}')
+print(f'Last name length: {len(last_name)}')
+#4 Declare 5 as num_one and 4 as num_two
+num_one = 5
+
+num_two = 2
+# Add num_one and num_two and assign the value to a variable total
+print(num_one + num_two)
+    # Subtract num_two from num_one and assign the value to a variable diff
+print(num_one - num_two)
+    # Multiply num_two and num_one and assign the value to a variable product
+print(num_one * num_two)    
+    # Divide num_one by num_two and assign the value to a variable division
+print(num_one / num_two)
+    # Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+print(num_one % num_two)
+    # Calculate num_one to the power of num_two and assign the value to a variable exp
+print(num_one ** num_two)   
+    # Find floor division of num_one by num_two and assign the value to a variable floor_division
+print(num_one // num_two)
+
+#The radius of a circle is 30 meters.
+
+radius_circle = 30
+
+#Calculate the area of a circle and assign the value to a variable name of area_of_circle
+
+area = 3.14 * radius_circle**2
+print(area)
+#Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circumference = 3.14*radius_circle
+print(circumference)
+#Take radius as user input and calculate the area.
+
+radius_circle_2 = input(int)
+
+area_2 = 3.14 * radius_circle_2 **2
+
+print(area_2)
